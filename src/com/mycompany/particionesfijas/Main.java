ae4a4a2c8d78bd849fbf624e9e3ec801015fff01
LPSoldier19 <rafael.bautista1@hotmail.es>
2021-02-12 17:51:04 -0600

    Proyecto particiones estaticas fijas

---
+++ src/com/mycompany/particionesfijas/Main.java
@@ -0,0 +1,18 @@
+package com.mycompany.particionesfijas;
+
+/**
+ *
+ * @author LPSoldier19
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventanaParticionesFijas f = new ventanaParticionesFijas();
+        f.setVisible (true);
+
+    }
+    
+}
